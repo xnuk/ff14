6f3df4bbaa5cf2f971f15d1dac8a0bc8a969dfdb
Xnuk Shuman <limm1127@gmail.com>
2017-07-22 12:24:47 +0900

    robocopy

--- mkdir.bat
+++ mkdir.bat
@@ -1,2 +1,3 @@
 mkdir "%USERPROFILE%\Documents\My Games\FINAL FANTASY XIV - KOREA\"
+robocopy .\ "%USERPROFILE%\Documents\My Games\FINAL FANTASY XIV - KOREA\" /s /e
 explorer "%USERPROFILE%\Documents\My Games\FINAL FANTASY XIV - KOREA\"
